2c0ee5a41bb487e4300c6472f1fb95591e0f9b08
Evgenii Monin <cucubitter@gmail.com>
2022-12-07 17:26:39 +0200

    Settings windows
    - настройки чарактеров

---
+++ Assets/Game/Dev/NpcSettingsController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Game.Characters;
+using Game.Characters.Npc;
+using Game.Stats;
+using Game.Stats.Impl;
+using UnityEngine;
+
+namespace Game.Dev
+{
+    public class NpcSettingsController : MonoBehaviour
+    {
+        public NpcController template;
+        public NpcController[] npcs;
+        
+        [Space]
+        public ParamView healthMaxValue;
+        public ParamView healthRestoreAmount;
+        public ParamView healthRestorePeriod;
+
+        [Space]
+        public ParamView staminaMaxValue;
+        public ParamView staminaRestoreAmount;
+        public ParamView staminaRestorePeriod;
+        
+        [Space]
+        public ParamView speed;
+        public ParamView jumpSpeed;
+        
+        private List<HealthIntBehaviour> _healths;
+        private List<StaminaIntBehaviour> _staminas;
+        private List<ValueIntRecovery> _healthRestores;
+        private List<ValueIntRecovery> _staminaRestores;
+        private List<MoveSetting> _moves;
+        private List<JumpSetting> _jumps;
+
+        private void Awake()
+        {
+            npcs = FindObjectsOfType<NpcController>();
+
+            _healths = npcs.Select(npc => npc.Health).ToList();
+            _staminas = npcs.Select(npc => npc.Stamina).ToList();
+
+            _moves = npcs.Select(npc => npc.MoveSetting).ToList();
+            _jumps = npcs.Select(npc => npc.JumpSetting).ToList();
+
+            _healthRestores = _healths.Select(health => health.GetComponent<ValueIntRecovery>()).ToList();
+            _staminaRestores = _staminas.Select(stamina => stamina.GetComponent<ValueIntRecovery>()).ToList();
+        }
+
+        private void Start()
+        {
+            UpdateViews();
+
+            healthMaxValue.sliderView.onValueChanged.AddListener(t => _healths.ForEach(h => h.MaxValue = (int)t));
+            healthRestoreAmount.sliderView.onValueChanged.AddListener(t => _healthRestores.ForEach(h => h.RecoveryAmount = (int)t));
+            healthRestorePeriod.sliderView.onValueChanged.AddListener(t => _healthRestores.ForEach(h => h.RecoveryPeriod = t));
+            
+            staminaMaxValue.sliderView.onValueChanged.AddListener(t => _staminas.ForEach(s => s.MaxValue = (int)t));
+            staminaRestoreAmount.sliderView.onValueChanged.AddListener(t => _staminaRestores.ForEach(s => s.RecoveryAmount = (int)t));
+            staminaRestorePeriod.sliderView.onValueChanged.AddListener(t => _staminaRestores.ForEach(s => s.RecoveryPeriod = t));
+            
+            speed.sliderView.onValueChanged.AddListener(t => _moves.ForEach(m=>m.speedMax = t));
+            jumpSpeed.sliderView.onValueChanged.AddListener(t => _jumps.ForEach(j => j.speed = t));
+        }
+
+        public void UpdateViews()
+        {
+            var health = template.Health;
+            var stamina = template.Stamina;
+
+            var move = template.MoveSetting;
+            var jump = template.JumpSetting;
+            
+            var healthRestore = health.GetComponent<ValueIntRecovery>();
+            var staminaRestore = stamina.GetComponent<ValueIntRecovery>();
+            
+            healthMaxValue.value = health.MaxValue;
+            healthRestoreAmount.value = healthRestore.RecoveryAmount;
+            healthRestorePeriod.value = healthRestore.RecoveryPeriod;
+            
+            staminaMaxValue.value = stamina.MaxValue;
+            staminaRestoreAmount.value = staminaRestore.RecoveryAmount;
+            staminaRestorePeriod.value = staminaRestore.RecoveryPeriod;
+            
+            speed.value = move.speedMax;
+            jumpSpeed.value = jump.speed;
+        }
+    }
+}
---
+++ Assets/Game/Dev/PlayerSettingsController.cs
@@ -0,0 +1,88 @@
+﻿using CucuTools;
+using CucuTools.Attributes;
+using Game.Characters;
+using Game.Characters.Player;
+using Game.Stats;
+using Game.Stats.Impl;
+using UnityEngine;
+
+namespace Game.Dev
+{
+    public class PlayerSettingsController : CucuBehaviour
+    {
+        public PlayerController player;
+        
+        [Space]
+        public ParamView healthMaxValue;
+        public ParamView healthRestoreAmount;
+        public ParamView healthRestorePeriod;
+
+        [Space]
+        public ParamView staminaMaxValue;
+        public ParamView staminaRestoreAmount;
+        public ParamView staminaRestorePeriod;
+        
+        [Space]
+        public ParamView speed;
+        public ParamView jumpSpeed;
+        
+        private HealthIntBehaviour _health;
+        private StaminaIntBehaviour _stamina;
+        private ValueIntRecovery _healthRestore;
+        private ValueIntRecovery _staminaRestore;
+        private MoveSetting _move;
+        private JumpSetting _jump;
+
+        private void Awake()
+        {
+            _health = player.Health;
+            _stamina = player.Stamina;
+
+            _move = player.MoveSetting;
+            _jump = player.JumpSetting;
+            
+            _healthRestore = _health.GetComponent<ValueIntRecovery>();
+            _staminaRestore = _stamina.GetComponent<ValueIntRecovery>();
+        }
+
+        private void Start()
+        {
+            UpdateViews();
+
+            healthMaxValue.sliderView.onValueChanged.AddListener(t => _health.MaxValue = (int)t);
+            healthRestoreAmount.sliderView.onValueChanged.AddListener(t => _healthRestore.RecoveryAmount = (int)t);
+            healthRestorePeriod.sliderView.onValueChanged.AddListener(t => _healthRestore.RecoveryPeriod = t);
+            
+            staminaMaxValue.sliderView.onValueChanged.AddListener(t => _stamina.MaxValue = (int)t);
+            staminaRestoreAmount.sliderView.onValueChanged.AddListener(t => _staminaRestore.RecoveryAmount = (int)t);
+            staminaRestorePeriod.sliderView.onValueChanged.AddListener(t => _staminaRestore.RecoveryPeriod = t);
+
+            speed.sliderView.onValueChanged.AddListener(t => _move.speedMax = t);
+            jumpSpeed.sliderView.onValueChanged.AddListener(t => _jump.speed = t);
+        }
+
+        [CucuButton()]
+        public void UpdateViews()
+        {
+            _health = player.Health;
+            _stamina = player.Stamina;
+
+            _move = player.MoveSetting;
+            _jump = player.JumpSetting;
+            
+            _healthRestore = _health.GetComponent<ValueIntRecovery>();
+            _staminaRestore = _stamina.GetComponent<ValueIntRecovery>();
+            
+            healthMaxValue.value = _health.MaxValue;
+            healthRestoreAmount.value = _healthRestore.RecoveryAmount;
+            healthRestorePeriod.value = _healthRestore.RecoveryPeriod;
+            
+            staminaMaxValue.value = _stamina.MaxValue;
+            staminaRestoreAmount.value = _staminaRestore.RecoveryAmount;
+            staminaRestorePeriod.value = _staminaRestore.RecoveryPeriod;
+            
+            speed.value = _move.speedMax;
+            jumpSpeed.value = _jump.speed;
+        }
+    }
+}
